0559bd9bef4775416e945c9e65b38bda67ca8882
Md. Anamul Hasan <mdanamulhasan.cse@gmail.com>
2023-05-18 02:22:57 +0600

    navbar added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Layout/Main';
+import Home from './pages/Home/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div className='container mx-auto'>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  </div>
 )
---
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import ActiveLink from "../../ActiveLink/ActiveLink";
+
+
+const Navbar = () => {
+    const navItems = <>
+        <li><ActiveLink to='/'>Home</ActiveLink></li>
+        <li><ActiveLink to='/allToys'>All Toys</ActiveLink></li>
+        <li><ActiveLink to='/myToys'>My Toys</ActiveLink></li>
+        <li><ActiveLink to='/addToys'>Add toys</ActiveLink></li>
+        <li><ActiveLink to='/blog'>Blog</ActiveLink></li>
+    </>
+    return (
+        <div className="navbar bg-base-200 ">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {navItems}
+                    </ul>
+                </div>
+                <Link to='/' className="btn btn-ghost normal-case text-xl">Toy Car <img src="" alt="" /></Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {navItems}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <button className="btn btn-active btn-primary mr-5">Login</button>
+                <button className="btn btn-outline btn-primary">Register</button>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
