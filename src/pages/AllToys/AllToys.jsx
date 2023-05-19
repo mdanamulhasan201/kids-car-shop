da15a0e34d92317e208365801cbc93e17a5754c2
Md. Anamul Hasan <mdanamulhasan.cse@gmail.com>
2023-05-19 16:56:20 +0600

    my car data show

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -5,6 +5,8 @@ import Cars from "./Cars";
 const AllToys = () => {
 
     const [cars, setCars] = useState([]);
+    const [search, setSearch] = useState('')
+
 
 
     useEffect(() => {
@@ -17,13 +19,21 @@ const AllToys = () => {
 
     return (
         <div>
-            <h3 className="font-bold text-center text-3xl my-10"> All Toys </h3>
+            <h3 className="font-bold text-center text-3xl my-10"> All Cars </h3>
+            <div className="form-control text-center mb-10">
+                <div className="input-group justify-center">
+                    <input onChange={(e) => setSearch(e.target.value)} type="text" placeholder="Search…" className="input input-bordered" />
+                    <button className="btn btn-square">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                    </button>
+                </div>
+            </div>
             {
-                cars.map((car) => ( 
-                <Cars car={car} key={car._id}>
-                
-                </Cars>
-                
+                cars.map((car) => (
+                    <Cars car={car} key={car._id}>
+
+                    </Cars>
+
                 ))
             }
         </div>
